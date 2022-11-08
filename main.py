391a752c66c107e9bee4871c84873a5248df0237
Thibault <49438882+Thibault-Cart@users.noreply.github.com>
2022-11-08 16:00:00 +0100

    publie sur ds

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import discord
 from menu import Recevoir_menu
 
+TOKEN = "MTAzOTUxODc3MTQzMTM2MjU5Mg.GGeiof.C2e74AyGxzpMl3vOL98HNlTqYhhWBWBy8sqbyM"
 
 
+intents = discord.Intents.all()
 
-for i in Recevoir_menu():
-    print(f"Bonjour, le menu de {i[0]} est :\n ")
-    print(f"--{i[1]}  \n prix: {i[2]} , {i[3]}")
-    print(f"--{i[4]}  \n prix: {i[5]} , {i[6]}")
-    print(f"--{i[7]}  \n prix: {i[8]} , {i[9]}")
-    print(f"--{i[10]} \n prix: {i[11]} , {i[12]}\n")
+client = discord.Client(intents=intents)
 
 
+@client.event
+async def on_ready():
+    print("Le bot est prêt !")
 
 
+@client.event
+async def on_message(message):
+    if (message.content == "--menu"):
+        print("toto\n")
+        menu = ""
+        channel = message.channel
+        for i in Recevoir_menu():
+            menu += f"\n le menu de {i[0]} est :\n "
+            menu += f"\n:hamburger:{i[1]}  \n :money_with_wings: {i[2]} , {i[3]}"
+            menu += f"\n:hamburger:{i[4]}  \n :money_with_wings: {i[5]} , {i[6]}"
+            menu += f"\n:hamburger:{i[7]}  \n :money_with_wings: {i[8]} , {i[9]}"
+            menu += f"\n:hamburger:{i[10]} \n :money_with_wings: {i[11]} , {i[12]}\n\n\n"
+            await channel.send(menu)
+            menu = ""
 
+client.run(TOKEN)
--- menu.py
+++ menu.py
@@ -4,28 +4,32 @@ import requests
 
 
 def Recevoir_menu():
-    # Making a GET request
+    # Récuperation des données sur le serveur
     r = requests.get(
         'http://cafe-battelle.eldora.ch/menus-du-jour/cafe-battelle/')
-
+    #recupération du code html/css/js
     html: str = r.text
-
+    #mise du html dans un objet soup afin de la manipuler 
     soup = BeautifulSoup(html, "html.parser")
+    #tuple qui contiendra les menus de la semaine
     tuple_repas = [["lundi"], ["Mardi"], ["Mercredi"], ["Jeudi"], ["Vendredi"]]
     no_jour = -1
-
+    #pour chaque div ayant pour class "dayCover track", est egual a chaque jour
     for x in soup.find_all("div", attrs={'class': 'dayCover track'}):
         no_jour += 1
-        # print(x,"\n\n")
-
+        #retransforme le code correspondant au jour en objet beautifull soup
         toto = BeautifulSoup(str(x), "html.parser")
+        #pour chaque div ayant comme class ""
         for y in toto.find_all("div", attrs={'class': 'col-md-5 col-sm-5 col-xs-10 col-xs-offset-1 menuPlat text-left'}):
+            ##retransforme le code correspondant a un repas du jour en objet soupe
+
             titi = BeautifulSoup(str(y), "html.parser")
 
-            switch_menu_prix = 0
+            #passe en revu le contenu des  balise p
             for z in titi.find_all("p"):
 
                 text: str = str(z)
+                #enleve les restant de code html
                 text = text.replace("<p>", "")
                 text = text.replace('<p class="text-right menuPrix">', "")
                 text = text.replace("<p", "")
@@ -34,6 +38,7 @@ def Recevoir_menu():
                 text = text.replace('<br>', " ")
                 text = text.replace('</br>', " ")
                 text = text.replace('<br/>', " ")
+                #ajoute au repas du jour
                 tuple_repas[no_jour].append(text)
 
             for y in toto.find_all("div", attrs={'class': 'col-md-5 col-sm-5 col-xs-10 col-xs-offset-1 menuPlat text-left col-md-offset-1 col-sm-offset-1'}):
